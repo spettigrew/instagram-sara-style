9f9a5b2e2f8eb0339ae17ca062ab8dbfed59b992
spettigrew <sara.pettigrew5@gmail.com>
2020-04-17 12:14:35 -0600

    Server setup with statuses

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors =  require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+require('dotenv').config()
+
+server.get('/', (req, res, next) => {
+    return res.status(201).json({ message: "Welcome to Instaproject, Sara-style!" })
+})
+
+server.use((err, req, res, next) => {
+    console.log("Error", err)
+    return res.status(500).json({ message: "Please try again." })
+})
+
+module.exports = server
