6284960ceb34f072d779d81fa93a65109537f62b
spettigrew <sara.pettigrew5@gmail.com>
2020-04-20 13:54:54 -0600

    Post model complete

---
+++ posts/post-model.js
@@ -0,0 +1,45 @@
+const db = require("../config")
+
+module.exports = {
+    posts,
+    findBy,
+    findById,
+    insert,
+    update,
+    remove,
+}
+
+function posts() {
+    return db ('posts')
+    .select('name')
+}
+
+function findBy() {
+    return db('posts')
+    .where(filter)
+}
+
+function findById() {
+    return db('posts')
+    .where({ id })
+    .first()
+}
+
+function insert(post) {
+    const [ id ] = db ('posts')
+    .insert(post)
+    return findById(id)
+}
+
+function update(id, changes) {
+    db('posts')
+    .where({ id })
+    .update(changes)
+    return findById(id)
+}
+
+function remove(id) {
+    return db('posts')
+    .where({ id })
+    .del()
+}
