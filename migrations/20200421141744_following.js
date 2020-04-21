df5678daa623af2ef8a5afb40d49e402ed043e0f
spettigrew <sara.pettigrew5@gmail.com>
2020-04-21 15:43:08 -0600

    Username changed to name

--- migrations/20200421141744_following.js
+++ migrations/20200421141744_following.js
@@ -3,7 +3,7 @@ exports.up = async function(knex) {
     await knex.schema.createTable("following", (table) => {
         table.increments('id')
         table.integer('user_id').unique()
-        table.text('username').notNull().unique()
+        table.text('name').notNull()
     })
 };
 
