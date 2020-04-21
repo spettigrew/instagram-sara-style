ece818c1ef1d3a0039ed8baaf27ba402e360633b
spettigrew <sara.pettigrew5@gmail.com>
2020-04-21 14:23:28 -0600

    Following migration setup

---
+++ migrations/20200421141744_following.js
@@ -0,0 +1,12 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable("following"), (table) => {
+        table.increments('id')
+        table.integer('user_id').unique()
+        table.text('username').notNull().unique()
+    }
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("following")
+};
