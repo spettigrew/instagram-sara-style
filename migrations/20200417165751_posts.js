0d42bf804aa3f92ec4b7e1b625e860b603ab44ce
spettigrew <sara.pettigrew5@gmail.com>
2020-04-21 14:23:04 -0600

    NotNull removed from user_id

--- migrations/20200417165751_posts.js
+++ migrations/20200417165751_posts.js
@@ -2,7 +2,7 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('posts', (table) => {
       table.increments('id')
-      table.integer('user_id').notNull().unique()
+      table.integer('user_id').unique()
       table.text('caption')
   })
 };
