6b6adcedf6bfa6b155a5cd37cef80dd886b282b4
Sara Pettigrew <sarapettigrew@Saras-MacBook-Pro.local>
2020-05-06 18:39:10 -0600

    Moved/deleted files

--- index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./server')
-
-const PORT = process.env.PORT || 3000
-
-if(!module.parent) {
-    server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`)
-    })
-}
